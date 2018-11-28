04ba6cdc789065eda39d45eb4320185d0d839775
t7haki01 <t7haki01@students.oamk.fi>
2018-11-28 11:50:17 +0200

    few more payload added for the getting data for using in client side

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ var jwtOptions = {
   /**And here comes the option i set only expire time based on npm doc many options like algorithm,
    * notbefore and so on */
   //For developing test, set as 30 sec
-  expiresIn: 600,
+  expiresIn: 120,
 
   issuer: 'SurveyApp',
   audience: 'http://localhost:3001/',
@@ -62,51 +62,54 @@ app.post('/log-in', (req, res) => {
   const { account, password } = req.body;
   console.log('User submitted: ', account, password);
   var sql = require('./database');
-  sql.query('select * from account where account = ?', [account], function(
-    error,
-    results,
-    fields
-  ) {
-    if (error) {
-      res.json({
-        status: false,
-        message: 'there are some error with query'
-      });
-    } else {
-      if (results.length > 0) {
-        console.log(privateKEY);
-        bcrypt.compare(password, results[0].password, function(err, ress) {
-          if (!ress) {
-            res.json({
-              status: false,
-              message: 'Account and password does not match'
-            });
-          } else {
-            //here set json web token with npm package account, secret key words with expire time
-            let token = jwt.sign(
-              /**Here is the payload */ { account: account },
-
-              /**and here is my secret or private key normally it would be difficult unrecongized random text */
-              /* 'ouluOnKaupunki' */ privateKEY,
-              jwtOptions
-            );
-            res.json({
-              status: true,
-              message: 'Successfully Login',
-              id: results[0].id,
-              err: null,
-              token
-            });
-          }
-        });
-      } else {
+  sql.query(
+    'select account.account as account, account.password as password, user.id as id, user.email as email, user.type as type from account inner join user on account.id=user.accountFK where account = ?',
+    [account],
+    function(error, results, fields) {
+      if (error) {
         res.json({
           status: false,
-          message: 'Account does not exits'
+          message: 'there are some error with query'
         });
+      } else {
+        if (results.length > 0) {
+          bcrypt.compare(password, results[0].password, function(err, ress) {
+            if (!ress) {
+              res.json({
+                status: false,
+                message: 'Account and password does not match'
+              });
+            } else {
+              //here set json web token with npm package account, secret key words with expire time
+              let token = jwt.sign(
+                /**Here is the payload */ {
+                  account: account,
+                  id: results[0].id,
+                  email: results[0].email,
+                  type: results[0].type
+                },
+
+                /**and here is my secret or private key normally it would be difficult unrecongized random text */
+                /* 'ouluOnKaupunki' */ privateKEY,
+                jwtOptions
+              );
+              res.json({
+                status: true,
+                message: 'Successfully Login',
+                err: null,
+                token
+              });
+            }
+          });
+        } else {
+          res.json({
+            status: false,
+            message: 'Account does not exits'
+          });
+        }
       }
     }
-  });
+  );
 });
 
 app.get('/exjwt', jwtMW /* Using the express jwt MW here */, (req, res) => {
