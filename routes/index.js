cb33288678bee9822db045fffaadb15621091de8
t7haki01 <t7haki01@students.oamk.fi>
2018-12-13 20:22:18 +0200

    readme and index and survey route edited first

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
-    title: 'Express API Hello World!/nWelcome To Survey Application API!'
+    title: 'Express API Hello World!' + "<br>" + 'Welcome To Survey Application API!'
   });
 });
 module.exports = router;
